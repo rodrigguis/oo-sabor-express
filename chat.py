b5bca87d0d4452ba24a7a9954085b25f513e0f18
rodrigguis <rodrigguis@gmail.com>
2024-06-04 12:08:15 -0300

    PR ajustes pslint

--- chat.py
+++ chat.py
@@ -1,30 +1,29 @@
-
+"""Module providing a function exit."""
 import sys
-from langchain_core.prompts import ChatPromptTemplate 
-from langchain_openai import ChatOpenAI 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
 model = ChatOpenAI(model="gpt-4o", temperature=0)
 
 sent_messages =[]
 
-print(f' Ola, eu sou o Superbot, e sou um especialista em musica Rap.')
+print('Ola, eu sou o Superbot, e sou um especialista em musica Rap.')
 while True:
     messages = [
-        ('system', 'Ola, sou o SuperBot eu sou especialista em musica Rap.'), 
+        ('system', 'Ola, sou o SuperBot eu sou especialista em musica Rap.'),
         *sent_messages,
-        ('user', '{user_input}'), 
+        ('user', '{user_input}')
     ]
 
     print(messages)
-    
     prompt = ChatPromptTemplate.from_messages(messages)
 
     chain = messages | model
 
     user_input = input('>')
 
-    if user_input == 'q': 
-        import sys; sys.exit(0)
+    if user_input == 'q':
+        sys.exit(0)
 
     response_ai = model.invoke({'user_input' : user_input})
 
